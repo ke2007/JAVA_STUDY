2b0b2c88ffeb3488c46199de26c76236e8f7a3a0
dlawn <dlawnsrjs@gmail.com>
2022-05-05 23:00:48 +0900

    비교연산자

---
+++ src/비교연산자.java
@@ -0,0 +1,13 @@
+public class 비교연산자 {
+    public static void main(String[] args) {
+        System.out.println("a");
+        if (false) {
+            System.out.println(1);
+        } else if (true) {
+            System.out.println(2);
+        } else {
+            System.out.println("b");
+        }
+
+    }
+}
