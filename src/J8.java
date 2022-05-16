460e12d91941de3b2bf6567f7ec3bcf8ccc502c4
dlawn <dlawnsrjs@gmail.com>
2022-05-16 14:23:16 +0900

    study5/16 메소드 출력

--- src/J8.java
+++ src/J8.java
@@ -1,16 +1,23 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class J8 {
 
 
-    public static void main(String[]/*문자열배열*/ args) {
+    public static void main(String[]/*문자열배열*/ args) throws IOException {
 
-        printTwoTimes("B"/* 인자, argument*/,"-");
-
+//        writeFileTwoTimes("a", "*");
+        System.out.println(twoTimes("a", "-"));
+        FileWriter fw = new FileWriter("out.txt");
+        fw.write(twoTimes("a", "*"));
+        fw.close();
 
     }
-    public static void printTwoTimes(String text/* 메소드안으로 정보는 보내는 매개변수, parameter*/, String delimiter) {
-
-        System.out.println(delimiter);
-        System.out.println(text);
-        System.out.println(text);
+    public static String twoTimes(String text, String delimiter) {
+        String out = "a";
+        out = out + delimiter + "\n"; // out -> a-\n
+        out = out + text + "\n";      // out -> a-\na\n
+        out = out + text + "\n";      // out -> a-\na\na\n
+        return out;
     }
 }
---
+++ src/J9.java
@@ -0,0 +1,14 @@
+public class J9 {
+    public static String a() {
+        return "a";
+    }
+    public static int one() {
+        return 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(a());
+        System.out.println(one());
+
+    }
+}
